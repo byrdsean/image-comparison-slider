42c453027d3d7fc2494b23da3a1bb36ec62726c6
Sean Byrd <byrd.sean1015@gmail.com>
2023-07-20 22:57:42 -0400

    Added functionality to support vertical comparison. Refactored javascript.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,77 @@
 window.addEventListener("load", (event) => {
-  const leftPane = document.getElementById("leftImgPane");
-  const rightPane = document.getElementById("rightImgPane");
-  const rangeControl = document.getElementById("rangeControl");
+  const getSliderDomObjects = () => {
+    return {
+      slider: document.getElementById("sliderContainer"),
+      leftPane: document.getElementById("leftImgPane"),
+      rightPane: document.getElementById("rightImgPane"),
+      rangeControl: document.getElementById("rangeControl"),
+    };
+  };
+
+  const { slider, leftPane, rightPane, rangeControl } = getSliderDomObjects();
+
+  const isVertical = slider.hasAttribute("vertical");
+  const showSlider = () => slider.classList.add("show");
 
   //Reset the width of the left and right image containers
   rangeControl.addEventListener("input", (e) => {
-    leftPane.style.width = `${e.target.value}%`;
-    rightPane.style.width = `${100 - e.target.value}%`;
+    const property = isVertical ? "height" : "width";
+    leftPane.style[property] = `${e.target.value}%`;
+    rightPane.style[property] = `${100 - e.target.value}%`;
   });
 
-  const setSliderWidth = () => {
+  const setSliderDimensions = (height, width) => {
+    const separationWidth = 1;
+    const margin = 7;
+    const extraSpace = 2 * margin + 2 * separationWidth;
+
+    slider.style.height = `${height + extraSpace}px`;
+    slider.style.width = `${width + extraSpace}px`;
+
+    if (isVertical) {
+      slider.style.height = `${height}px`;
+      rangeControl.style.width = `${height + extraSpace}px`;
+
+      const imgContainer = document.getElementById("sliderImgContainer");
+      imgContainer.style.height = `${height - separationWidth}px`;
+      imgContainer.style.width = `${width}px`;
+    }
+  };
+
+  const getImage = (source) => {
+    const img = new Image();
+    img.src = source;
+    return img;
+  };
+
+  const setError = (errorMessage) => {
+    slider.classList.add("error");
+    console.error(errorMessage);
+    showSlider();
+  };
+
+  const displaySlider = () => {
     const leftImg = leftPane.querySelector("img");
     const rightImg = rightPane.querySelector("img");
-
     if (!leftImg || !rightImg) {
-      console.error(
-        "ERROR: One or more of the required images has not been set."
-      );
+      setError("One or more of the required images has not been set.");
       return;
     }
 
-    const leftImageObj = new Image();
-    leftImageObj.src = leftImg.src;
+    const leftImageSrc = getImage(leftImg.src);
+    const rightImageSrc = getImage(rightImg.src);
 
-    const righttImageObj = new Image();
-    righttImageObj.src = rightImg.src;
-
-    const slider = document.getElementById("sliderContainer");
-    if (leftImageObj.width !== righttImageObj.width) {
-      slider.className = `${slider.className} error`.trim();
-      console.error(
-        "ERROR: The two images provided are different sizes. Will display error message instead."
-      );
+    const isHeightEqual = leftImageSrc.height === rightImageSrc.height;
+    const isWidthEqual = leftImageSrc.width === rightImageSrc.width;
+    if (isHeightEqual && isWidthEqual) {
+      setSliderDimensions(leftImageSrc.height, leftImageSrc.width);
+      showSlider();
     } else {
-      //Adding 16px to account for the left/right range margin and 1px border
-      //left margin + right margin + left border + right margin = 7+7+1+1 = 16
-      slider.style.width = `${leftImageObj.width + 16}px`;
+      setError(
+        "The two images provided are different sizes. Will display error message instead."
+      );
     }
   };
-  setSliderWidth();
+
+  displaySlider();
 });
